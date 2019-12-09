87b0d8294c198cd378d467333da6928b4d664b33
Chaitanya <chaitanyas0101@gmail.com>
2019-12-09 11:58:23 +0530

    score and goal system has been made

--- main.gd
+++ main.gd
@@ -6,6 +6,9 @@ extends Spatial
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
+	set_process(true)
+		
 	pass # Replace with function body.
 func _process(delta):
 	if Input.is_action_just_pressed("ui_end"):
@@ -13,3 +16,9 @@ func _process(delta):
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_Area_body_entered(body):
+	if body is RigidBody:
+		print("win")
+	pass # Replace with function body.
